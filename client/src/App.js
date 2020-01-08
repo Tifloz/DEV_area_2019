3c849619cbeeb1df2a1826df49ccba12cc2365d7
Sebastien <sebastien.nardy@epitech.eu>
2020-01-08 09:40:45 +0100

    Front-> Add page login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Components/Login'
+import SignIn from "./Components/Login";
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+      <Router>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/login" component={SignIn} />
+      </Router>
   );
 }
 
