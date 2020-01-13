4591dc9be0c265c9e8dc6b1ae491c9b3350dd114
Ouaked <sabri.ouaked@epitech.eu>
2020-01-13 16:49:46 +0100

    [+] js comment

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -6,6 +6,7 @@ require('firebase/firestore')
  * @param {Object}  Result object
  * @param {integer} Code status
  * @param {string}  Message who will be send
+ * @return {status}  Json Response
  * This function return the result formatted
  */
 function result(res, code, message) {
@@ -17,11 +18,65 @@ function result(res, code, message) {
 /**
  * @param {Object}  Request object
  * @param {Object}  Result object
+ * @return {status}  200 Connected
+ * @return {status}  400 Access denied
  * Display home page
  */
-exports.homePage = async (req, res) => {
+exports.homePage = (req, res) => {
     const user = firebase.auth().currentUser
     if (!user)
         return result(res, 400, 'Please SignIn')
+    return result(res, 200, 'welcome to home page!')
+}
+
+/**
+ * @param {Object}  Request object
+ * @param {Object}  Result object
+ * @returns {status} 200 Success
+ * @returns {status} 400 Error not logged
+ * Create Task for user logged
+ */
+exports.createTask = (req, res) => {
+    const user = firebase.auth().currentUser
+    if (!user)
+        return result(res, 400, 'Error user not logged');
+    return result(res, 200, 'welcome to home page!')
+}
+
+/**
+ * @param {uid} User uid
+ * @returns {Array} tasks id for user
+ * @returns {null} no tasks
+ */
+function getUserTasks(uid)
+{
+    let db = firebase.firestore()
+    let result = db.collection('Users').doc(uid).get()
+        .then((result) => {
+            const tasks = result.data().tasks
+            if (tasks) {
+                return (tasks);
+            }
+        })
+        .catch ((err) => {
+            console.log('no documents');
+            return (null);
+        })
+}
+
+/**
+ * @param {Object}  Request object
+ * @param {Object}  Result object
+ * @returns {status}  400 not logged
+ * @returns {status}  200 task generated
+ * @returns {status}  401 task not generated
+ * Create Task for user logged
+ */
+exports.createTask = (req, res) => {
+    const user = firebase.auth().currentUser
+    if (!user)
+        return result(res, 400, 'Error user not logged')
+    const { service_id, actions, reactions } = req.body
+    firebase.firestore().doc
     return result(res, 200, 'welcome to home page!')
 }
