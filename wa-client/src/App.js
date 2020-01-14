7cd0a1d379da883012f876de64b13c7687614a71
Lucas <lucas.duboisse@epitech.eu>
2020-01-14 18:06:30 +0100

    [ADD] login page

--- wa-client/src/App.js
+++ wa-client/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import Dashboard from './Components/Dashboard';
-import SignIn from "./Components/SignIn";
+// import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
 import './css/App.css';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
+import Login from "./auth/Login";
 
 function App() {
   return(
       <Router>
-        <Route exact path="/" component={SignIn} />
-        <Route exact path="/signIn" component={SignIn} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signIn" component={Login} />
         <Route exact path="/signUp" component={SignUp} />
         <Route exact path="/dashboard" component={Dashboard} />
       </Router>
