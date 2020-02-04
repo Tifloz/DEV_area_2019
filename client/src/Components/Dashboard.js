a625a83f205d65522451915c44c5aaf4109465bc
sebastien <sebastien.nardy@epitech.eu>
2020-02-04 10:34:39 +0100

    [+] Front main page

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,21 +1,19 @@
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { withStyles} from "@material-ui/core/styles";
-import Container from '@material-ui/core/Container';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-
 import React from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import PropTypes from "prop-types";
-import {Redirect} from 'react-router-dom'
-import api from '../api'
 
 function Copyright() {
     return (
@@ -31,72 +29,121 @@ function Copyright() {
 }
 
 const styles = theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
+    icon: {
+        marginRight: theme.spacing(2),
     },
-    paper: {
-        marginTop: theme.spacing(8),
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
     },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
     },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
+    cardContent: {
+        flexGrow: 1,
     },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
     },
-    error: {
-        backgroundColor: theme.palette.error.dark,
-    },
-    noAccountLink: {
-        marginTop: theme.spacing(2),
-    }
 });
 
-//const classes = useStyles();
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
-class dashboard extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-        };
-    }
-
+class Dashboard extends React.Component {
     render() {
         const { classes } = this.props;
-        if (this.state.redirect) {
-            return <Redirect to='/dashboard'/>;
-        }
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Welcome to dashboard
-                </Typography>
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-        </Container>
-        )
+        return (
+            <React.Fragment>
+                <CssBaseline/>
+                <AppBar position="relative">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            AREA
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <main>
+                    {/* Hero unit */}
+                    <div className={classes.heroContent}>
+                        <Container maxWidth="sm">
+                            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                                AREA
+                            </Typography>
+                            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                                Start connectiong your world.
+                            </Typography>
+                            <div className={classes.heroButtons}>
+                                <Grid container spacing={2} justify="center">
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">
+                                            Get More
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Container>
+                    </div>
+                    <Container className={classes.cardGrid} maxWidth="md">
+                        {/* End hero unit */}
+                        <Grid container spacing={4}>
+                            {cards.map(card => (
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card className={classes.card}>
+                                        <CardMedia
+                                            className={classes.cardMedia}
+                                            image="https://source.unsplash.com/random"
+                                            title="Image title"
+                                        />
+                                        <CardContent className={classes.cardContent}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                Heading
+                                            </Typography>
+                                            <Typography>
+                                                This is a media card. You can use this section to describe the content.
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small" color="primary">
+                                                View
+                                            </Button>
+                                            <Button size="small" color="primary">
+                                                Edit
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Container>
+                </main>
+                {/* Footer */}
+                <footer className={classes.footer}>
+                    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                        Coucou ceci est un footer
+                    </Typography>
+                    <Copyright/>
+                </footer>
+                {/* End footer */}
+            </React.Fragment>
+        );
     }
 }
 
-dashboard.propTypes = {
+Dashboard.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(dashboard);
+export default withStyles(styles)(Dashboard);
