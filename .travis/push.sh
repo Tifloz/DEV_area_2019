4838074efd9805aaff8799025b222935c7f4d5b0
Florian Louvet <florian.louvet@epitech.eu>
2020-02-06 16:00:36 +0100

    [~] Maybe fixed travis this time (so boring u_u)

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
-git config --global user.email 'travis@travis-ci.org'
-git config --global user.name 'Travis'
-git remote set-branches --add origin master
-git fetch
-git reset --hard
-git checkout master
-git merge --ff-only "$TRAVIS_COMMIT"
-git push git+ssh://git@github.com/Tifloz/DEV_area_2019.git master
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis BOT"
+  # Keep track of where Travis put us.
+  # We are on a detached head, and we need to be able to go back to it.
+  local build_head=$(git rev-parse HEAD)
+
+  # Fetch all the remote branches. Travis clones with `--depth`, which
+  # implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+  # do that.
+  git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+  git fetch
+  git reset --hard
+  # create the tacking branches
+  for branch in $(git branch -r|grep -v HEAD) ; do
+      git checkout -qf ${branch#origin/}
+  done
+
+  # finally, go back to where we were at the beginning
+  git checkout ${build_head}
+}
+
+setup_git
+
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+git checkout master || exit
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+git push git+ssh://git@github.com/${TRAVIS_REPO_SLUG}.git master
