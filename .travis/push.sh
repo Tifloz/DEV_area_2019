397b8eff927e8953018a42071c93533cabc0aa1c
Florian Louvet <florian.louvet@epitech.eu>
2020-02-26 18:02:53 +0100

    [~] Fixed unit tests

--- .travis/push.sh
+++ .travis/push.sh
@@ -28,4 +28,5 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git checkout master || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
-git push git+ssh://git@github.com/${TRAVIS_REPO_SLUG}.git master
+git remote set-url --add --push origin git@git.epitech.eu:/florian.louvet@epitech.eu/DEV_area_2019
+git push master
