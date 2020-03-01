82a6fa40ea439690187a0e35f0059b7b3576f0bd
Georges Rached <georges.rached@epitech.eu>
2020-03-01 01:43:02 +0100

    [+] still working on API Google and Facebook

--- mobile/Components/Login.js
+++ mobile/Components/Login.js
@@ -3,13 +3,41 @@ import {WebView} from 'react-native-webview';
 import {
   View,
 } from 'react-native';
+// import { GoogleSignin, GoogleSigninButton } from '@react-native-community/google-signin';
+
+
+// import * as React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+
+// class Login extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Welcome to the login screen!</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+// export default Login;
+
+
 
 class Login extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
         <WebView
-          source={{uri: 'https://api.imgur.com/oauth2/authorize?client_id=4024d2a2fe8e4ce&response_type=token\n'}}
+          // source={{uri: 'https://api.imgur.com/oauth2/authorize?client_id=4024d2a2fe8e4ce&response_type=token\n'}}
           onNavigationStateChange={(webViewState) => this._onNavigationStateChange(webViewState)}
         />
       </View>
