fee04707de5d99f28872714898ba9f3e4585486f
Gautier Plancq <gautier.plancq@epitech.eu>
2020-03-13 18:04:54 +0100

    [+] add function to get all infos from a youtube channel => api/YoutubeAPI.js

--- server/api/YoutubeAPI.js
+++ server/api/YoutubeAPI.js
@@ -1,44 +1,44 @@
-import axios from 'axios';
+const api = require('../tools/api')
 const KEY = 'AIzaSyDIAnIjssl1frALsG1ePwwj4TBBZGxrSfQ';
 
-GET = 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key={AIzaSyDIAnIjssl1frALsG1ePwwj4TBBZGxrSfQ}'
+exports.getChannelInfos = async (channelId) => {
+    let url = 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=' + channelId + '&key=' + KEY;
+    return api.makeRequest(url, "GET", {}).then((result) => {
+        console.log(result)
+        return result
+    }).catch((err) => {
+        console.log(err)
+        return false
+    })
+};
 
-export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY
-    }
-})
+// export function buildApiRequest(requestMethod, path, params, properties) {
+//     params = removeEmptyParams(params);
+//     let request;
+//     if (properties) {
+//         let resource = createResource(properties);
+//         request = window.gapi.client.request({
+//             'body': resource,
+//             'method': requestMethod,
+//             'path': path,
+//             'params': params
+//         });
+//     } else {
+//         request = window.gapi.client.request({
+//             'method': requestMethod,
+//             'path': path,
+//             'params': params
+//         });
+//     }
+//     return request;
+// }
 
-export function buildApiRequest(requestMethod, path, params, properties) {
-    params = removeEmptyParams(params);
-    let request;
-    if (properties) {
-        let resource = createResource(properties);
-        request = window.gapi.client.request({
-            'body': resource,
-            'method': requestMethod,
-            'path': path,
-            'params': params
-        });
-    } else {
-        request = window.gapi.client.request({
-            'method': requestMethod,
-            'path': path,
-            'params': params
-        });
-    }
-    return request;
-}
-
-export function buildChannelRequest(channelId) {
-    return buildApiRequest(GET,
-        '/youtube/v3/channels',
-        {
-            part: 'snippet,statistics',
-            id: channelId,
-            fields: 'kind,items(id,snippet(description,thumbnails/medium,title),statistics/subscriberCount)'
-        }, null);
-}
+// export function buildChannelRequest(channelId) {
+//     return buildApiRequest(GET,
+//         '/youtube/v3/channels',
+//         {
+//             part: 'snippet,statistics',
+//             id: channelId,
+//             fields: 'kind,items(id,snippet(description,thumbnails/medium,title),statistics/subscriberCount)'
+//         }, null);
+// }
