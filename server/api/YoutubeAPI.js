a89fe2abbab09f1acd27b8b821919d75869183a5
Georges Rached <georges.rached@epitech.eu>
2020-03-13 17:36:43 +0100

    [+] added channel request on api youtube with snippet and statistics

--- server/api/YoutubeAPI.js
+++ server/api/YoutubeAPI.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 const KEY = 'AIzaSyDIAnIjssl1frALsG1ePwwj4TBBZGxrSfQ';
 
+GET = 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key={AIzaSyDIAnIjssl1frALsG1ePwwj4TBBZGxrSfQ}'
+
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
-        part : 'snippet',
-        maxResults : 5,
-        key : KEY
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
     }
 })
+
+export function buildChannelRequest(channelId) {
+    return buildApiRequest(GET,
+        '/youtube/v3/channels',
+        {
+            part: 'snippet,statistics',
+            id: channelId,
+            fields: 'kind,items(id,snippet(description,thumbnails/medium,title),statistics/subscriberCount)'
+        }, null);
+}
