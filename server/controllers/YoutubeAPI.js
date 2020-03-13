d42128b0c29d2bf28479cbb6843babb1251c3680
Georges Rached <georges.rached@epitech.eu>
2020-03-13 13:56:36 +0100

    [+] added library axios to simple fetch data youtube API key

--- server/controllers/YoutubeAPI.js
+++ server/controllers/YoutubeAPI.js
@@ -1,36 +1,48 @@
-var express = require('express');
-var app = express();
-var passport = require('passport');
-app.use(passport.initialize());
-var YoutubeV3Strategy = require('passport-youtube-v3').Strategy;
+// var express = require('express');
+// var app = express();
+// var passport = require('passport');
+// app.use(passport.initialize());
+// var YoutubeV3Strategy = require('passport-youtube-v3').Strategy;
 
-passport.use(new YoutubeV3Strategy({
-    clientID: YOUR_CLIENT_ID,
-    clientSecret: YOUR_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8081/redirect',
-    scope: ['https://www.googleapis.com/auth/youtube']
-},
-function (accessToken, refreshToken, profile, cb) {
-    var user = {
-        accessToken: accessToken,
-        refreshToken: refreshToken
-    };
-    return cb(null, user)
-}
-));
+// passport.use(new YoutubeV3Strategy({
+//     clientID: YOUR_CLIENT_ID,
+//     clientSecret: YOUR_CLIENT_SECRET,
+//     callbackURL: 'http://localhost:8081/redirect',
+//     scope: ['https://www.googleapis.com/auth/youtube']
+// },
+// function (accessToken, refreshToken, profile, cb) {
+//     var user = {
+//         accessToken: accessToken,
+//         refreshToken: refreshToken
+//     };
+//     return cb(null, user)
+// }
+// ));
 
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
-});
+// passport.serializeUser(function(user, cb) {
+//     cb(null, user);
+// });
 
-passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb) {
+//     cb(null, obj);
+// });
 
-app.get('/authenticate', passport.authenticate('youtube'))
-app.get('/redirect', passport.authenticate('youtube', { failureRedirect: '/login' }),
-function(req, res) {
-    res.redirect('http://localhost:8081' + '?access_token=' + req.user.accessToken)
+// app.get('/authenticate', passport.authenticate('youtube'))
+// app.get('/redirect', passport.authenticate('youtube', { failureRedirect: '/login' }),
+// function(req, res) {
+//     res.redirect('http://localhost:8081' + '?access_token=' + req.user.accessToken)
+// })
+
+// app.listen(8081)
+
+import axios from 'axios';
+const KEY = 'AIzaSyDIAnIjssl1frALsG1ePwwj4TBBZGxrSfQ';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part : 'snippet',
+        maxResults : 5,
+        key : KEY
+    }
 })
-
-app.listen(8081)
