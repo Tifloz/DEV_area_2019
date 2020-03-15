7ff563c43e8b1b2a89495ffd81f5f1fdb0f61cc2
LucasDbs <lucas.duboisse@epitech.eu>
2020-03-15 22:52:17 +0100

    [FIX] ServicePanel

--- wa-client/src/CreateArea/ServicePanel.js
+++ wa-client/src/CreateArea/ServicePanel.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
